cf4b5dd439bfdf2e96d3805b62161f651f2fd80c
Rapha <rafhaelsouza245@gmail.com>
2023-11-26 23:19:36 -0300

    sab

--- Exercício - Sabrina/servidor.js
+++ Exercício - Sabrina/servidor.js
@@ -1,15 +1,31 @@
 const express = require ('express')
 const app = express()
+const fs = require('fs')
+
 app.use(express.static('public'))
 app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+const PORT = 8080
 
 app.get("/", (requisicao, resposta) => {
-    resposta.render('desk1')
+    resposta.render('Inicio')
 })
 app.get("/de", (requisicao, resposta) => {
-    resposta.render('desk2')
+    resposta.render('Portal')
 })
 app.get("/desk", (requisicao, resposta) => {
-    resposta.render('desk3')
+    resposta.render('Inscrição')
+})
+app.post('/salvar', (req, res) => {
+    dados = {
+        nome: req.body.nome,
+        sobrenome: req.body.sobrenome,
+        senha: req.body.pass,
+        Email: req.body.email,
+        Nascimento: req.body.dia,
+    }
+    fs.appendFileSync('usuario.json', `\n${JSON.stringify(dados)}`)
+    resultado = `Olá, ${dados}`
+    res.render('inscrição', { resultado })
 })
 app.listen(8080)
